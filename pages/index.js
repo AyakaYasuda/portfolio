9baa7bf2818fd1660cbdb75f12c61200656b61cc
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-06-29 00:14:09 -0700

    added title and meta description

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import Head from 'next/head';
+
 const HomePage = () => {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Head>
+        <title>Ayaka Yasuda | Software Developer in Vancouver</title>
+        <meta
+          name="description"
+          content="Ayaka Yasuda is a software developer who specializes in developing web applications with React and Node.js. Based in Vancouver."
+        />
+      </Head>
+    </>
+  );
 };
 
 export default HomePage;
