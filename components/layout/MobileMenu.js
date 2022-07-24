1d979d443c793e4f9f5f02f63ffc046c8a06c0a6
Ayaka Yasuda <ayakayasuda@Ayakas-MacBook-Air.local>
2022-07-24 05:48:56 -0700

    changed url of google document

--- components/layout/MobileMenu.js
+++ components/layout/MobileMenu.js
@@ -26,7 +26,7 @@ const MobileMenu = ({ drawerState, onToggle }) => {
             MENU
           </li>
           <li className="NavLink col-start-7 FlexCenter gap-1 lg:col-start-8">
-            <a href="https://drive.google.com/uc?export=download&id=1QNzBvobAsr0jd0uMPA0sDuGAUjl_jsiA" download>
+            <a href="https://drive.google.com/uc?export=download&1uY4ToNH3-cbSI85G6fUVpyoxbuKGkOI7zRNqZpyegdQ" download>
               RESUME
             </a>
             <DownloadIcon sx={{ color: '#FFFFFF', fontSize: 18 }} />
